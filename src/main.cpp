30a9a2c788620c25e5213be8a19964926959e575
Marcus Carter <marcoose408@gmail.com>
2025-03-20 21:45:41 -0400

    crapton of header files - glm

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 #include <GLFW/glfw3.h>
 #include <stb_image.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 // #define STB_IMAGE_IMPLEMENTATION
 #include <shader.h>
 
